7568f257ab04e5abf4984e2be79b82616849a589
Harry Sibbenga <sibbengaharry@gmail.com>
2025-12-28 09:46:10 +0000

    feat: export MetricsCards from calculator index

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -1,6 +1,6 @@
 export { default as SliderInput } from './SliderInput.vue'
 export { default as OdometerDisplay } from './OdometerDisplay.vue'
+export { default as MetricsCards } from './MetricsCards.vue'
 export { default as LineChart } from './LineChart.vue'
 export { default as OptimizationToggle } from './OptimizationToggle.vue'
 export { default as LeadGate } from './LeadGate.vue'
-
