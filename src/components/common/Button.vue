47245f6067aa0b06ab68b068e7cebfcb81c4565f
Harry Sibbenga <sibbengaharry@gmail.com>
2025-12-28 07:59:24 +0000

    feat: create shared UI components (Button, Card)

---
+++ src/components/common/Button.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+    :class="['btn', `btn-${variant}`, { 'btn-full': fullWidth }]"
+    :disabled="disabled"
+    :type="type"
+    @click="$emit('click', $event)"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  variant: {
+    type: String,
+    default: 'primary',
+    validator: (value) => ['primary', 'secondary'].includes(value)
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  fullWidth: {
+    type: Boolean,
+    default: false
+  },
+  type: {
+    type: String,
+    default: 'button'
+  }
+})
+
+defineEmits(['click'])
+</script>
+
+<style scoped>
+.btn-full {
+  width: 100%;
+}
+</style>
+
---
+++ src/components/common/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="['card', { 'card-elevated': elevated }]">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  elevated: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
